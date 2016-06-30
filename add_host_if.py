fccb8f01646c515b7a515004a9fdf20023782abe
Rugen, Len <rugenl@missouri.edu>
2016-06-30 13:39:41 -0500

    Add host interface

---
+++ add_host_if.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+#
+# add_host_if.py Add host interfaces for vhost web tests
+#
+# Parms 1) target host name
+#       2) interface dns name
+#       3) port
+#
+# Generate 
+
+from pyzabbix import ZabbixAPI
+
+import json
+import sys
+import socket
+from operator import itemgetter
+
+# activate these lines for tracing
+#import logging
+#logging.basicConfig(filename='pyzabbix_debug.log',level=logging.DEBUG)
+
+# The hostname at which the Zabbix web interface is available
+ZABBIX_SERVER = 'https://.....................'
+
+zapi = ZabbixAPI(ZABBIX_SERVER)
+
+# Login to the Zabbix API
+zapi.login('Admin', 'zabbix')
+
+# Command line arg is the host to process
+target_host = str(sys.argv[1])
+dns = str(sys.argv[2])
+port = str(sys.argv[3])
+
+# get the target host 
+host = zapi.host.get(filter={"host":target_host}, output="extend", selectInterfaces="extend")
+#print json.dumps(host, indent=4, sort_keys=True)
+
+if len(host) > 0:
+    host = host[0]
+    hostId = host['hostid']
+
+for interface in host['interfaces']:
+    if dns == interface["dns"]:
+       print interface["dns"]
+       sys.exit( 'Interface exists, update in GUI' )
+
+ip = socket.gethostbyname(dns) 
+
+print 'Adding to', target_host, dns, ip, port
+zapi.hostinterface.create(hostid=hostId,
+	dns=dns,
+	ip=ip,
+	main='0',
+	port=port,
+	type='1',
+        useip='0',
+	)
+	
+
